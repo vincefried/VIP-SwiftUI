4aba093873aa9bc42ea960d9adc8ff9ae6631594
Vincent Friedrich <vincefried@web.de>
2023-01-14 15:58:17 +0100

    Move counter view into subview

--- VIPExample/Feature/FeatureView.swift
+++ VIPExample/Feature/FeatureView.swift
@@ -16,60 +16,30 @@ protocol FeatureInteracting {
 struct FeatureView: View {
 
     let interactor: FeatureInteracting?
-    @ObservedObject var viewState: ViewState
+    let viewState: ViewState
 
     var body: some View {
-        HStack(spacing: 20) {
-            Button {
-                interactor?.handleDidTapMinusButton(request: .init())
-            } label: {
-                Image(systemName: "minus.circle.fill")
+        FeatureCounterView(interactor: interactor, viewState: viewState.counterViewState)
+            .padding()
+            .onAppear {
+                interactor?.handleOnAppear(request: .init())
             }
-
-            Text(viewState.labelText)
-
-            Button {
-                interactor?.handleDidTapPlusButton(request: .init())
-            } label: {
-                Image(systemName: "plus.circle.fill")
-            }
-        }
-        .font(.largeTitle)
-        .fontDesign(.rounded)
-        .padding()
-        .onAppear {
-            interactor?.handleOnAppear(request: .init())
-        }
     }
 
-    final class ViewState: ObservableObject, FeatureDisplaying {
+    final class ViewState: FeatureDisplaying {
 
-        @Published var labelText: String = ""
+        @Published var counterViewState = FeatureCounterView.ViewState()
 
         func displayOnAppear(viewModel: Feature.OnAppear.ViewModel) {
-            labelText = viewModel.labelText
+            counterViewState.labelText = viewModel.labelText
         }
 
         func displayDidTapMinusButton(viewModel: Feature.DidTapMinusButton.ViewModel) {
-            labelText = viewModel.labelText
+            counterViewState.labelText = viewModel.labelText
         }
 
         func displayDidTapPlusButton(viewModel: Feature.DidTapPlusButton.ViewModel) {
-            labelText = viewModel.labelText
+            counterViewState.labelText = viewModel.labelText
         }
     }
-}
-
-struct FeatureView_Previews: PreviewProvider {
-    static var previews: some View {
-        FeatureView(interactor: nil, viewState: .example)
-    }
-}
-
-extension FeatureView.ViewState {
-    static let example: FeatureView.ViewState = {
-        let viewState = FeatureView.ViewState()
-        viewState.labelText = "3"
-        return viewState
-    }()
 }
--- VIPExample/Feature/View/FeatureCounterView.swift
+++ VIPExample/Feature/View/FeatureCounterView.swift
@@ -8,15 +8,44 @@
 import SwiftUI
 
 struct FeatureCounterView: View {
-    
+    let interactor: FeatureInteracting?
+    @ObservedObject var viewState: ViewState
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 20) {
+            Button {
+                interactor?.handleDidTapMinusButton(request: .init())
+            } label: {
+                Image(systemName: "minus.circle.fill")
+            }
+
+            Text(viewState.labelText)
+
+            Button {
+                interactor?.handleDidTapPlusButton(request: .init())
+            } label: {
+                Image(systemName: "plus.circle.fill")
+            }
+        }
+        .font(.largeTitle)
+        .fontDesign(.rounded)
+    }
+
+    final class ViewState: ObservableObject {
+        @Published var labelText: String = ""
     }
 }
 
 struct FeatureCounterView_Previews: PreviewProvider {
     static var previews: some View {
-        FeatureCounterView()
+        FeatureCounterView(interactor: nil, viewState: .example)
     }
+}
+
+extension FeatureCounterView.ViewState {
+    static let example: FeatureCounterView.ViewState = {
+        let viewState = FeatureCounterView.ViewState()
+        viewState.labelText = "3"
+        return viewState
+    }()
 }
