b1ba3ddc0034bb5bb5990e957138b53b9ae9b555
Vincent Friedrich <vincefried@web.de>
2023-01-14 15:47:08 +0100

    WIP

---
+++ VIPExample/Feature/View/FeatureCounterView.swift
@@ -0,0 +1,22 @@
+//
+//  FeatureCounterView.swift
+//  VIPExample
+//
+//  Created by Vincent Friedrich on 14.01.23.
+//
+
+import SwiftUI
+
+struct FeatureCounterView: View {
+    
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeatureCounterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeatureCounterView()
+    }
+}
---
+++ VIPExample/Feature/View/FeatureCounterViewState.swift
@@ -0,0 +1,12 @@
+//
+//  FeatureCounterViewModel.swift
+//  VIPExample
+//
+//  Created by Vincent Friedrich on 14.01.23.
+//
+
+import Foundation
+
+final class FeatureCounterViewState: ObservableObject {
+    @Published var labelText: String = ""
+}
